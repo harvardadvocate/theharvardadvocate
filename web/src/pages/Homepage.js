4c3612e406984ff602352c5b14c856ae2d65b3c0
Isabelle <isa.zheng@gmail.com>
2022-03-27 11:30:12 -0400

    implement header frame thingy

--- web/src/pages/Homepage.js
+++ web/src/pages/Homepage.js
@@ -3,15 +3,43 @@ import React, { useEffect, useState } from "react";
 import { Themed } from "theme-ui";
 
 const homepageSx = {
-  margin: "0.5em",
-  width: "90%",
+  margin: "2em 5% 2em 0",
+  ".header": {
+    marginLeft: "20px",
+    borderBottom: "1px solid #000",
+  },
   ".horizontalContainer": {
     display: "flex",
-    borderTop: "1px solid #000",
+    width: "100%",
+    marginTop: "2em",
+    ".mainContent": {
+      flexGrow: 1,
+    },
   },
-  ".verticalLines": {},
-  ".topVL": {},
-  ".mainContent": {},
+  ".verticalLines": {
+    width: "20px",
+    marginRight: "40px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "40px",
+
+    ".topVL": {
+      borderRight: "1px solid #000",
+      width: "20px",
+      height: "250px",
+      h5: {
+        lineHeight: 1,
+        writingMode: "vertical-rl",
+        textOrientation: "mixed",
+        transform: "rotate(180deg)",
+      },
+    },
+    ".bottomVL": {
+      borderRight: "1px solid #000",
+      width: "20px",
+      flexGrow: 1,
+    },
+  },
 };
 
 // TODO: pull out "frame" for pages including margin
@@ -24,7 +52,9 @@ export default function Homepage() {
       </div>
       <div className="horizontalContainer">
         <div className="verticalLines">
-          <div className="topVL"></div>
+          <div className="topVL">
+            <Themed.h5>MAGAZINE</Themed.h5>
+          </div>
           <div className="bottomVL"></div>
         </div>
         <div className="mainContent"></div>
--- web/src/pages/Section.js
+++ web/src/pages/Section.js
@@ -6,7 +6,58 @@ import { Themed } from "theme-ui";
 import rightArrow from "../assets/images/right-arrow.svg";
 
 const sectionSx = {
-  margin: "2em 5%",
+  margin: "2em 5% 2em 0 ",
+  ".header": {
+    marginLeft: "20px",
+    display: "flex",
+    ".headerImgWrapper": {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      margin: "0 0.5em",
+    },
+    ".headerLeft": {
+      paddingRight: "3em",
+      borderBottom: "1px solid #000",
+    },
+    ".headerRight": {
+      borderBottom: "1px solid #000",
+      flexGrow: 1,
+    },
+    img: { height: "0.4em" },
+  },
+  ".horizontalContainer": {
+    width: "100%",
+    display: "flex",
+    marginTop: "2em",
+    ".mainContent": {
+      flexGrow: 1,
+    },
+  },
+  ".verticalLines": {
+    width: "20px",
+    marginRight: "40px",
+    display: "flex",
+    flexDirection: "column",
+    gap: "40px",
+
+    ".topVL": {
+      borderRight: "1px solid #000",
+      width: "20px",
+      height: "250px",
+      h5: {
+        lineHeight: 1,
+        writingMode: "vertical-rl",
+        textOrientation: "mixed",
+        transform: "rotate(180deg)",
+      },
+    },
+    ".bottomVL": {
+      borderRight: "1px solid #000",
+      width: "20px",
+      flexGrow: 1,
+    },
+  },
   ".sectionHeader": {
     fontStyle: "italic",
     borderBottom: "1px solid #000",
@@ -39,11 +90,35 @@ export default function Section(props) {
 
   return (
     <div sx={sectionSx}>
-      <div className="sectionHeader">
-        <Themed.h2>{props.section}</Themed.h2>
-        <img src={rightArrow} alt="right-arrow" />
+      <div className="header">
+        {
+          //TODO: these should link, also pull this stuff out
+        }
+        <div className="headerLeft">
+          <Themed.h5 sx={{ margin: "0.5em" }}>Sections</Themed.h5>
+        </div>
+        <div className="headerImgWrapper">
+          <img src={rightArrow} alt="right-arrow" />
+        </div>
+        <div className="headerRight">
+          <Themed.h5 sx={{ margin: "0.5em" }}>{props.section}</Themed.h5>
+        </div>
       </div>
-      <ContentItemList items={items} />
+      <div className="horizontalContainer">
+        <div className="verticalLines">
+          <div className="topVL">
+            <Themed.h5>MAGAZINE</Themed.h5>
+          </div>
+          <div className="bottomVL"></div>
+        </div>
+        <div className="mainContent">
+          <div className="sectionHeader">
+            <Themed.h2>{props.section}</Themed.h2>
+            <img src={rightArrow} alt="right-arrow" />
+          </div>
+          <ContentItemList items={items} />
+        </div>
+      </div>
     </div>
   );
 }
