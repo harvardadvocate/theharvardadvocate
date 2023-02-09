f9925df7772172347b4488df9c40e877a011efa4
let985 <33074399+leahteich@users.noreply.github.com>
2023-02-09 01:18:38 -0500

    fixed responsive for submit, subscribe

--- web/src/pages/Submit.js
+++ web/src/pages/Submit.js
@@ -6,11 +6,12 @@ import { useLocation, Link } from "react-router-dom";
 
 const submitSx = {
   ".submitBody": {
-    marginTop: "0.4em",
+    margin: "auto",
+    width: "60%",
     display: "flex",
     flexDirection: "column",
-    alignItems:"center",
-    margin: "0em 15em 2em 15em",
+    flexWrap: "wrap",
+    alignItems: "center",
   },
   i: {
     textAlign: "center",
@@ -54,8 +55,13 @@ const submitSx = {
     ".submitBody": {
       margin: "0em 0em 0em 0em",
       marginTop: "1em",
+      margin:"auto",
+      width: "80%",
+      flexDirection: "column",
+      flexWrap: "wrap",
     },
   },
+
 
 };
 
@@ -75,7 +81,7 @@ export default function Submit() {
           The Harvard Advocate accepts submissions of art, fiction, and poetry of any form or length created by members of the Harvard community, as well as outside contributors at our discretion. All submissions are considered anonymously.
           <br/><br/>
           <hr/>
-          <h2 align="center">Spring 2023 Issue</h2>
+          <Themed.h2 align="center">Spring 2023 Issue</Themed.h2>
           <hr/>
           <i align="center">Our winter 2023 Review Period ends on Friday, October 14th, 2022.</i>
           <br/>
