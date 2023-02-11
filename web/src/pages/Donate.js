466be6026dd205da55e0222122b1257b0a371afd
Andreas Lordos <andreas@dhcp-10-250-148-240.harvard.edu>
2023-02-11 13:48:45 -0500

    css changes, static pages look normal on all laptop screens + issuee list fixes

--- web/src/pages/Donate.js
+++ web/src/pages/Donate.js
@@ -9,7 +9,7 @@ const donateSx = {
     display: "flex",
     flexDirection: "column",
     alignItems:"center",
-    margin: "0em 15em 2em 15em",
+    margin: "0em 21vw 5vh 21vw",
   },
   i: {
     textAlign: "center",
