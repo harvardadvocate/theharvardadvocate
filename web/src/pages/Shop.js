3d8d46e57337ca2cbf8bd8d2a33178bfb3ee5591
blangman <blangman@college.harvard.edu>
2023-03-06 00:44:09 -0500

    a working integrated api (thank god)

--- web/src/pages/Shop.js
+++ web/src/pages/Shop.js
@@ -2,6 +2,8 @@
 import { Themed } from "theme-ui";
 import Frame from "../components/Frame";
 import rightArrow from "../assets/images/right-arrow.svg";
+import ShopifyBuy from 'shopify-buy';
+import React, { useEffect, useState } from "react";
 
 const shopSx = {
   ".shopBody": {
@@ -24,6 +26,24 @@ const shopSx = {
 };
 
 export default function Shop() {
+  const [products, setProducts] = React.useState([]);
+
+  useEffect(() => {
+    const client = ShopifyBuy.buildClient({
+      domain: 'the-harvard-advocate.myshopify.com',
+      storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
+    });
+
+    //PAT: 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
+
+    client.product.fetchAll().then((products) => {
+      setProducts(products);
+    }).catch((error) => {
+      console.error(error);
+    });
+  },
+    []);
+
   return (
     <div sx={shopSx}>
       <Frame
@@ -35,8 +55,12 @@ export default function Shop() {
         ]}
       >
         <div className="shopBody">
-          <Themed.p>
-          </Themed.p>
+          {products.map((product) => (
+            <div key={product.id}>
+              <Themed.p>{product.title}</Themed.p>
+              <img src={product.images[0].src} alt={product.title} />
+            </div>
+          ))}
         </div>
       </Frame>
     </div>
