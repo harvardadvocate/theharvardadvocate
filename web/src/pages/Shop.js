1bd3bfd90c873e28e0ed36bd9b5d18f978a8110e
blangman <blangman@college.harvard.edu>
2023-03-07 15:15:00 -0500

    add to cart, open cart

--- web/src/pages/Shop.js
+++ web/src/pages/Shop.js
@@ -5,15 +5,37 @@ import rightArrow from "../assets/images/right-arrow.svg";
 import ShopifyBuy from 'shopify-buy';
 import React, { useEffect, useState } from "react";
 
+
 const shopSx = {
   ".shopBody": {
-    marginTop: "0.4em",
-    marginLeft: "22%",
-    marginRight: "25%"
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "0 em",
+    justifyContent: "center",
+    justifyItems: "center",
+    textAlign: "center",
+    alignItems: "center",
+    figcaption: {
+      fontSize: "16px",
+    },
   },
   i: {
     textAlign: "center",
     display: "block"
+  },
+
+  ".productContainer": {
+    width: "60%",
+    marginBottom: "1em",
+  },
+
+  ".productImage": {
+    width: "100%",
+    marginBottom: "0.5em",
+  },
+
+  ".productButton": {
+    width: "100%",
   },
 
   "@media (max-width: 767px)": {
@@ -22,28 +44,50 @@ const shopSx = {
       marginTop: "1em",
     },
   },
-
 };
 
 export default function Shop() {
   const [products, setProducts] = React.useState([]);
+  const [cart, setCart] = useState(null);
 
-  useEffect(() => {
+  const addToCart = (variantId, quantity) => {
+    const lineItemsToAdd = [{ variantId, quantity }];
+    const checkoutId = cart.id;
     const client = ShopifyBuy.buildClient({
       domain: 'the-harvard-advocate.myshopify.com',
       storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
     });
-
-    //PAT: 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
-
-    client.product.fetchAll().then((products) => {
-      setProducts(products);
-    }).catch((error) => {
-      console.error(error);
+    client.checkout.addLineItems(checkoutId, lineItemsToAdd).then((checkout) => {
+      setCart(checkout);
+      window.location.href = checkout.webUrl;
     });
-  },
-    []);
+  };
 
+  const openCart = () => {
+    const client = ShopifyBuy.buildClient({
+      domain: 'the-harvard-advocate.myshopify.com',
+      storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
+    });
+    client.checkout.create().then((checkout) => {
+      window.open(checkout.webUrl);
+    });
+  };
+
+useEffect(() => {
+  const client = ShopifyBuy.buildClient({
+    domain: 'the-harvard-advocate.myshopify.com',
+    storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
+  });
+
+  //PAT: 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
+
+  client.product.fetchAll().then((products) => {
+    setProducts(products);
+  }).catch((error) => {
+    console.error(error);
+  });
+},
+  []);
   return (
     <div sx={shopSx}>
       <Frame
@@ -54,11 +98,16 @@ export default function Shop() {
           },
         ]}
       >
-        <div className="shopBody">
+        <div className="shopBody" sx={{ display: "flex", flexWrap: "wrap" }}>
           {products.map((product) => (
-            <div key={product.id}>
+            <div key={product.id} sx={{ width: "calc(33%)", margin: "0.5em" }}>
               <Themed.p>{product.title}</Themed.p>
               <img src={product.images[0].src} alt={product.title} />
+              <Themed.p>{product.price}</Themed.p>
+              <div data-element="product.buttonWrapper">
+                <button data-element="product.button" onClick={() => addToCart(product.variants[0].id, 1)}>ADD TO CART</button>
+              </div>
+              <hr />
             </div>
           ))}
         </div>
