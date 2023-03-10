1665421d7682f0ab8b4db32160bfd72ad359e196
blangman <blangman@college.harvard.edu>
2023-03-10 15:20:15 -0500

    save commit

---
+++ web/src/components/shopStyles.js
@@ -0,0 +1,35 @@
+const shopSx = {
+    ".shopBody": {
+      display: "grid",
+      gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+      alignItems: "flex-end",
+      padding: "1em",
+      gap: "1em",
+      scrollSnapType: "x mandatory",
+      scrollBehavior: "smooth",
+    },
+    ".product": {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+    },
+    "img": {
+      verticalAlign: "bottom",
+      maxWidth: "100%",
+      height: "auto",
+    },
+    "p": {
+      margin: "0",
+    },
+    ".productButton": {
+      width: "40%",
+    },
+    ".productCard": {
+      width: "30%",
+      scrollSnapAlign: "start",
+      margin: "0 1em",
+      textAlign: "center",
+    },
+  };
+  
+  export default shopSx;
--- web/src/pages/Shop.js
+++ web/src/pages/Shop.js
@@ -1,46 +1,9 @@
-/** @jsxImportSource theme-ui */
 import { Themed } from "theme-ui";
 import Frame from "../components/Frame";
 import ShopifyBuy from 'shopify-buy';
 import React, { useEffect, useState } from "react";
-
-
-const shopSx = {
-  ".shopBody": {
-    display: "flex",
-    flexWrap: "wrap",
-    marginTop: "0 em",
-    justifyContent: "center",
-    justifyItems: "center",
-    textAlign: "center",
-    alignItems: "center",
-  },
-  i: {
-    textAlign: "center",
-    display: "block"
-  },
-
-  ".productContainer": {
-    width: "60%",
-    marginBottom: "1em",
-  },
-
-  ".productImage": {
-    width: "100%",
-    marginBottom: "0.5em",
-  },
-
-  ".productButton": {
-    width: "100%",
-  },
-
-  "@media (max-width: 767px)": {
-    ".shopBody": {
-      margin: "0em 0em 0em 0em",
-      marginTop: "1em",
-    },
-  },
-};
+import { Helmet } from "react-helmet"
+import shopSx from "../components/shopStyles.js"
 
 export default function Shop() {
   const [products, setProducts] = React.useState([]);
@@ -48,42 +11,36 @@ export default function Shop() {
 
   const addToCart = (variantId, quantity) => {
     const lineItemsToAdd = [{ variantId, quantity }];
-    const checkoutId = cart.id;
-    const client = ShopifyBuy.buildClient({
-      domain: 'the-harvard-advocate.myshopify.com',
-      storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
-    });
-    client.checkout.addLineItems(checkoutId, lineItemsToAdd).then((checkout) => {
-      setCart(checkout);
-      window.location.href = checkout.webUrl;
-    });
-  };
-
-  const openCart = () => {
     const client = ShopifyBuy.buildClient({
       domain: 'the-harvard-advocate.myshopify.com',
       storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
     });
     client.checkout.create().then((checkout) => {
-      window.open(checkout.webUrl);
+      setCart(checkout);
+      return client.checkout.addLineItems(checkout.id, lineItemsToAdd);
+    }).then((checkout) => {
+      setCart(checkout);
+      window.location.href = checkout.webUrl;
+    }).catch((error) => {
+      console.log(error);
     });
   };
 
-useEffect(() => {
-  const client = ShopifyBuy.buildClient({
-    domain: 'the-harvard-advocate.myshopify.com',
-    storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
-  });
-  
-  client.product.fetchAll().then((products) => {
-    setProducts(products);
+  useEffect(() => {
+    const client = ShopifyBuy.buildClient({
+      domain: 'the-harvard-advocate.myshopify.com',
+      storefrontAccessToken: '005d55feb024fc1214eaf8b8dd90aad0'
+    });
+
+    client.product.fetchAll().then((products) => {
+      setProducts(products);
+      console.log(products)
+    }).catch((error) => {
+      console.log(error);
+    });
     console.log(products)
-  }).catch((error) => {
-    
-  });
-  console.log(products)
-},
-  []);
+  }, []);
+
   return (
     <div sx={shopSx}>
       <Frame
@@ -94,22 +51,18 @@ useEffect(() => {
           },
         ]}
       >
-        <div className="shopBody" sx={{ display: "flex", flexWrap: "wrap" }}>
-        
-          
+        <div className="shopBody">
           {products.map((product) => (
-            <div key={product.id} sx={{ width: "20em", margin: "0.5em" }}>
-              <Themed.p>
-              {product.title}
-              </Themed.p>
-              <img src={product.images[0].src} alt={product.title} />
-              <Themed.p>
-                ${parseFloat(product.variants[0].price.amount).toFixed(2)} 
-              </Themed.p>
-          
-              <div data-element="product.buttonWrapper">
-                <button data-element="product.button" onClick={() => openCart(product.variants[0].id, 1)}>ADD TO CART</button>
-              </div>
+            <div key={product.id} className="product">
+              <Themed.p>{product.title}</Themed.p>
+              <img src={product.images[0].src} alt={product.title} width="100%" />
+              <Themed.p>${parseFloat(product.variants[0].price.amount).toFixed(2)}</Themed.p>
+              <button
+                className="productButton"
+                onClick={() => addToCart(product.variants[0].id, 1)}
+              >
+                Add to Cart
+              </button>
               <hr />
             </div>
           ))}
@@ -118,4 +71,3 @@ useEffect(() => {
     </div>
   );
 }
-
