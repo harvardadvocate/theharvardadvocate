bb7e27c020535473723081e36fb426f5d8ecca8a
Conan Lu <conanlu4@gmail.com>
2025-02-02 23:31:09 -0500

    blog namechange fix

--- web/src/queries/homepage.js
+++ web/src/queries/homepage.js
@@ -144,52 +144,52 @@ const getFeaturedArt2 = `*[_type == "contentItem" && "artFeaturedBottomRight" in
                   }`;
 
 
-// const getBlog1 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[0] {
-//                     title,
-//                     authors[]->{name, slug},
-//                     issue->{title, slug},
-//                     slug,
-//                     body,
-//                     sections[]->{title, slug},
-//                     mainImage{
-//                       asset->{
-//                       _id,
-//                       url
-//                     }
-//                   }
-//                 }`;  
+const getBlog1 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[0] {
+                    title,
+                    authors[]->{name, slug},
+                    issue->{title, slug},
+                    slug,
+                    body,
+                    sections[]->{title, slug},
+                    mainImage{
+                      asset->{
+                      _id,
+                      url
+                    }
+                  }
+                }`;  
                 
                 
-// const getBlog2 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[1] {
-//                   title,
-//                   authors[]->{name, slug},
-//                   issue->{title, slug},
-//                   slug,
-//                   body,
-//                   sections[]->{title, slug},
-//                   mainImage{
-//                     asset->{
-//                     _id,
-//                     url
-//                   }
-//                 }
-//               }`;  
+const getBlog2 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[1] {
+                  title,
+                  authors[]->{name, slug},
+                  issue->{title, slug},
+                  slug,
+                  body,
+                  sections[]->{title, slug},
+                  mainImage{
+                    asset->{
+                    _id,
+                    url
+                  }
+                }
+              }`;  
 
 
-// const getBlog3 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[2] {
-//                 title,
-//                 authors[]->{name, slug},
-//                 issue->{title, slug},
-//                 slug,
-//                 body,
-//                 sections[]->{title, slug},
-//                 mainImage{
-//                   asset->{
-//                   _id,
-//                   url
-//                 }
-//               }
-//             }`;  
+const getBlog3 = `*[_type == "contentItem" && "featuredBlog" in featuredOptions] | order(publishedAt desc)[2] {
+                title,
+                authors[]->{name, slug},
+                issue->{title, slug},
+                slug,
+                body,
+                sections[]->{title, slug},
+                mainImage{
+                  asset->{
+                  _id,
+                  url
+                }
+              }
+            }`;  
 
 const getInstagram = `*[_type == "imageAsset" && picsFrom21SouthStreet == true]  | order(publishedAt desc) {
                       title,
@@ -223,7 +223,9 @@ const getResources = `
   "featuredArticle6": ${getFeaturedArticle6},
   "featuredArt1": ${getFeaturedArt1},
   "featuredArt2": ${getFeaturedArt2},
-
+  "blog1": ${getBlog1},
+  "blog2": ${getBlog2},
+  "blog3": ${getBlog3},
   "instagram": ${getInstagram},
   "archivedContent": ${getArchivedContent}
 }`;
